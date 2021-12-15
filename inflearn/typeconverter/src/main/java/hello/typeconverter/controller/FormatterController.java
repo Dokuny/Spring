5bf3e091e8eb14670536dc4e808779e76376e77c
Dokuny <nestour95@gmail.com>
2021-12-15 11:30:16 +0900

    study type Converter

---
+++ inflearn/typeconverter/src/main/java/hello/typeconverter/controller/FormatterController.java
@@ -0,0 +1,40 @@
+package hello.typeconverter.controller;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.NumberFormat;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.time.LocalDateTime;
+
+@Slf4j
+@Controller
+public class FormatterController {
+    @GetMapping("/formatter/edit")
+    public String formatterForm(Model model) {
+        Form form = new Form();
+        form.setNumber(10000);
+        form.setLocalDateTime(LocalDateTime.now());
+        log.info("number={}, date={}", form.getNumber(), form.getLocalDateTime());
+        model.addAttribute("form", form);
+        return "formatter-form";
+    }
+
+    @PostMapping("/formatter/edit")
+    public String formatterEdit(@ModelAttribute Form form) {
+        return "formatter-view";
+    }
+
+    @Data
+    static class Form {
+        @NumberFormat(pattern = "###,###")
+        private Integer number;
+        @DateTimeFormat(pattern = "yyyy년 MM월 dd일 HH시 mm분 ss초")
+        private LocalDateTime localDateTime;
+    }
+}
