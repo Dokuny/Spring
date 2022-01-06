82342395e5283389ccc368bf14ccf8056b9bbe94
Dokuny <nestour95@gmail.com>
2022-01-06 11:41:41 +0900

    connect backend server

--- project/TodoList/react-workspace/todo-react-app/src/AddTodo.js
+++ project/TodoList/react-workspace/todo-react-app/src/AddTodo.js
@@ -6,6 +6,7 @@ class AddTodo extends React.Component {
     constructor(props) {
         super(props);
         this.state = {item: {title: ""}};
+        this.add = props.add; // props 의 함수를 this.add에 연결
     }
 
     onInputChange = (e) =>{
@@ -15,15 +16,39 @@ class AddTodo extends React.Component {
         console.log(thisItem);
     }
 
+    onButtonClick = () => {
+        this.add(this.state.item);
+        this.setState({item:{title:""}});
+    }
+
+    enterKeyEventHandler = (e) =>{
+        if(e.key === 'Enter'){
+            this.onButtonClick();
+        }
+    }
+
     render() {
         return(
             <Paper style={{margin:16,padding:16}}>
                 <Grid container>
                     <Grid xs={11} md={11} item style={{paddingRight:16}}>
-                        <TextField placeholder="Add Todo here" fullWidth/>
+                        <TextField
+                            placeholder="Add Todo here"
+                            fullWidth
+                            onChange={this.onInputChange}
+                            value={this.state.item.title}
+                            onKeyPress={this.enterKeyEventHandler}
+                        />
                     </Grid>
                     <Grid xs={1} md={1} item>
-                        <Button fullWidth color="secondary" variant="outlined">+</Button>
+                        <Button
+                            fullWidth
+                            color="secondary"
+                            variant="outlined"
+                            onClick={this.onButtonClick}
+                        >
+                            +
+                        </Button>
                     </Grid>
                 </Grid>
             </Paper>
--- project/TodoList/react-workspace/todo-react-app/src/App.js
+++ project/TodoList/react-workspace/todo-react-app/src/App.js
@@ -1,34 +1,64 @@
 import React from 'react';
 import Todo from './Todo';
 import './App.css';
-import {List, Paper} from "@material-ui/core";
+import {Container, List, Paper} from "@material-ui/core";
 import AddTodo from "./AddTodo";
+import {call} from "./service/ApiService";
 
 class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            items: [
-                {id: "0", title: "hello world 1", done: true},
-                {id: "1", title: "hello world 2", done: false},
-            ],
+            items: [],
         };
+
+    }
+
+    add = (item) => {
+        call("/todo", "POST", item).then((response) =>
+            this.setState({items: response.data})
+        );
+    };
+
+    delete = (item) => {
+        call("/todo", "DELETE", item).then((response) =>
+            this.setState({items: response.data})
+        );
+    };
+
+    componentDidMount() {
+        call("/todo", "GET", null).then((response) =>
+            this.setState({items: response.data})
+        );
+    }
+
+    update = (item) => {
+        call("/todo", "PUT", item).then((response) =>
+            this.setState({items: response.data})
+        );
     }
 
     render() {
-        let todoItems = this.state.items.length> 0 &&(
-            <Paper style={{margin:16}}>
+        let todoItems = this.state.items.length > 0 && (
+            <Paper style={{margin: 16}}>
                 <List>
-                    {this.state.items.map((item,idx) => (
-                        <Todo item={item} key={item.id}/>
+                    {this.state.items.map((item, idx) => (
+                        <Todo
+                            item={item}
+                            key={item.id}
+                            delete={this.delete}
+                            update={this.update}
+                        />
                     ))}
                 </List>
             </Paper>
         )
         return (
             <div className="App">
-                <AddTodo />
-                <div className="TodoList">{todoItems}</div>
+                <Container maxWidth="md">
+                    <AddTodo add={this.add}/>
+                    <div className="TodoList">{todoItems}</div>
+                </Container>
             </div>
         );
     }
